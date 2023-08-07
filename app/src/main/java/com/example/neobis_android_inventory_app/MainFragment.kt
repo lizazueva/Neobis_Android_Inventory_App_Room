d8ee65377f65530945f0fe51451780e97817a928
Елизавета Зуева <elizavetazueva@MacBook-Air-Elizaveta.local>
2023-08-07 19:11:13 +0300

    Add BottomNavigationView

---
+++ app/src/main/java/com/example/neobis_android_inventory_app/MainFragment.kt
@@ -0,0 +1,20 @@
+package com.example.neobis_android_inventory_app
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.neobis_android_inventory_app.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
